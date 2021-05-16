735047d80ece0a19e4eff9209d069d813f3c870d
DevGoran <goran.vlacic@hotmail.com>
2021-05-16 12:08:54 +0200

    Adapter + Gradle improvements - Added ViewHolder pattern to the CityCategoryItemAdapter. - Gradle: Replaced 'junit:junit:4.+' with specific version.

--- app/src/main/java/com/example/tourguideapp/CityCategoryItemAdapter.java
+++ app/src/main/java/com/example/tourguideapp/CityCategoryItemAdapter.java
@@ -26,35 +26,46 @@ public class CityCategoryItemAdapter extends ArrayAdapter<CityCategoryItem> {
         super(context, 0, items);
     }
 
+    /**
+     * ViewHolder class.
+     */
+    static class ViewHolder {
+        private TextView title;
+        private TextView info;
+        private ImageView icon;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder;
         // Check if an existing view is being reused, otherwise inflate the view
-        View listItemView = convertView;
-        if (listItemView == null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
+            holder = new ViewHolder();
+            holder.title = convertView.findViewById(R.id.text_text_view);
+            holder.info = convertView.findViewById(R.id.info_text_view);
+            holder.icon = convertView.findViewById(R.id.icon);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
 
         // Get the CityCategoryItem object located at this position in the list.
         CityCategoryItem currentCityCategoryItem = getItem(position);
 
-        // Find the TextView in the list_item.xml layout with the ID text_text_view.
-        TextView textTextView = listItemView.findViewById(R.id.text_text_view);
         // Get the text from the currentCityCategoryItem object and set this text on the textTextView TextView.
-        textTextView.setText(currentCityCategoryItem.getText());
+        holder.title.setText(currentCityCategoryItem.getText());
 
-        // Find the TextView in the list_item.xml layout with the ID location_text_view.
-        TextView infoTextView = listItemView.findViewById(R.id.info_text_view);
         // Get the text from the currentCityCategoryItem object and set this text on the infoTextView TextView.
-        infoTextView.setText(currentCityCategoryItem.getInfoId());
+        holder.info.setText(currentCityCategoryItem.getInfoId());
 
-        // Find the ImageView in the list_item.xml layout with the ID icon.
-        ImageView icon = listItemView.findViewById(R.id.icon);
         // Get the icon from the currentCityCategoryItem object and set this image on the icon ImageView.
-        icon.setImageResource(currentCityCategoryItem.getIconResourceId());
+        holder.icon.setImageResource(currentCityCategoryItem.getIconResourceId());
 
         // Return the whole list item layout (containing 2 TextView's and 1 ImageView) so that it can be shown in
         // the ListView.
-        return listItemView;
+        return convertView;
     }
 }
